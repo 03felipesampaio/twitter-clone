1ed037e99481d5ee3a121e56fc38a567cd22c1b0
Felipe Sampaio <03.felipesampaio@gmail.com>
2021-04-05 23:30:42 -0300

    Add likes and RTs

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const fs = require("fs");
 
 const db = require('./src/database');
 
@@ -13,53 +12,142 @@ const user = {
   id: 'guest123'
 }
 
+// Home page
 app.get("/", (req, res) => {
-  const command = 'select tweet_id, author, name, date, content, rts, likes, cmnts from tweets inner join users on author = user_id'
-  db.query(command, [], (err, response) => {
+  const columns = 'id, author, name, date, content, rts, likes, cmmnts'
+  const command = `SELECT ${columns} FROM tweets inner join users on author = user_id`;
+  db.query(command, (err, response) => {
     if (err) {
+      console.error(err);
       return err;
     }
     const tweets = response.rows;
-
-    // res.status(200).send(tweets);
     res.render("home", { tweets: tweets.slice().reverse()});
   });
 });
+
+app.get('/teste', (req, res) => {
+  res.render('teste');
+})
 
 app.post("/", (req, res) => {
-  let body = req.body;
-
   // Create a new post and add to the list
-  const command = 'INSERT INTO tweets() VALUES ($1, $2, $3)';
-  const params = [req.body];
-  db.query(command, [], (err, response) => {
+  const command = 'INSERT INTO tweets(author, content) VALUES ($1, $2)';
+  const params = [user.id, req.body.content];
+  db.query(command, params, (err, response) => {
     if (err) {
+      console.error(err);
       return err;
     }
+
+    console.log('INSERTED 0 1');
+    res.redirect("/");
+  });
+});
+
+// Get a users page
+app.get("/users/:userID", (req, res) => {
+  // Create a new post and add to the list
+  const rows = 'id, author, name, date, content, rts, likes, cmmnts';
+  const command = `SELECT ${rows} FROM tweets inner join users on author = $1`;
+  const params = [req.params.userID];
+
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
     const tweets = response.rows;
 
-    // res.status(200).send(tweets);
-    res.render("home", { tweets: tweets.slice().reverse()});
+    // console.log(tweets[0].likes);
+
+    res.render('user', {tweets: tweets });
+  });
+});
+
+// Search for a tweet
+app.get("/tweets/:id", (req, res) => {
+  const command = `SELECT * FROM tweets WHERE id = $1`;
+  const params = [req.params.id];
+
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
+    const tweet = response.rows;
+
+    // console.log(tweets[0].likes);
+
+    res.send(tweet);
+  });
+});
+
+// Add like to a tweet
+app.post('/tweets/:id/like', (req, res) => {
+  const command = `UPDATE tweets SET likes = likes + 1 WHERE id = $1`;
+  const params = [req.params.id];
+
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
+    console.log(response);
+    res.status(200).send({message: 'LIKED TWEET'});
   });
 
-  res.redirect("/");
 });
 
-app.get("/users/:userID", (req, res) => {
-  const userID = req.params.userID;
+// Retweet
+app.post('/tweets/:id/rt', (req, res) => {
+  let command_ = `UPDATE $1 SET $2 = $3 + 1 WHERE $4 = $5`;
+  let params = ['tweets', 'rts', 'rts', 'id', req.params.id];
 
-  const userTweets = db.tweets.filter(twt => twt.user.author === userID);
+  // *** LEMBRAR DE FAZER UMA TRANSACAO RT -> ADD RT TABLE ***
+  db.query(command_, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
 
-  let profile = userTweets[0].user;
+    console.log('RT');
+  });
 
-  res.render('user', { user: profile, tweets: userTweets });
+  command_ = `INSERT INTO retweets(tweet_id, author) VALUES($1, $2)`;
+  params = [req.params.id, user.id];
+
+  // *** LEMBRAR DE FAZER UMA TRANSACAO RT -> ADD RT TABLE ***
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
+    console.log('RT SALVO');
+    res.status(200).send({message: 'RT adicionado com sucesso'});
+  });
+
+
 });
 
-app.post("/save", (req, res) => {
-  // Save file with tweets
-  let data = JSON.stringify(db);
-  fs.writeFileSync('defaultTweets.json', data);
-  res.redirect('/');
+// Delete a tweet
+app.post('/delete/:tweetID', (req, res) => {
+  const command = 'DELETE FROM tweets WHERE id = $1';
+  const params = [req.params.tweetID];
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
+    console.log('DELETED');
+    res.redirect("/");
+  });
+
 });
 
 app.listen(3000, () => {
