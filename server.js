4db4cfb0307b5ae8b2fa73e352fe082345199b75
Felipe Sampaio <03.felipesampaio@gmail.com>
2021-04-08 17:21:21 -0300

    Add like and rt functions on db

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { toggleLike } = require("./src/database");
 
 const db = require('./src/database');
 
@@ -10,7 +11,7 @@ app.use(express.static(__dirname + '/public'));
 app.use('/scripts', express.static(__dirname + '/public'));
 
 const user = {
-  id: undefined
+  id: 'guest123'
 }
 
 // *** Login page ***
@@ -19,6 +20,7 @@ app.get("/login", (req, res) => {
   res.render(  "login"  );
 });
 
+// Create a new user
 app.post('/login', (req, res) => {
   const USERID = req.body.user;
   const command = `SELECT * FROM users WHERE user_id = $1`;
@@ -45,6 +47,12 @@ app.post('/login', (req, res) => {
 
 // *** Home page ***
 app.get("/", (req, res) => {
+  if(!user.id) {
+    res.redirect("/login");
+    return;
+  }
+
+
   const columns = 'id, author, name, date, content, rts, likes, cmmnts'
   const command = `SELECT ${columns} FROM tweets inner join users on author = user_id`;
   db.query(command, (err, response) => {
@@ -57,8 +65,11 @@ app.get("/", (req, res) => {
   });
 });
 
+
+// *** POSTS ***
+
+// Create a new post and add to the list
 app.post("/", (req, res) => {
-  // Create a new post and add to the list
   const command = 'INSERT INTO tweets(author, content) VALUES ($1, $2)';
   const params = [user.id, req.body.content];
   db.query(command, params, (err, response) => {
@@ -72,65 +83,11 @@ app.post("/", (req, res) => {
   });
 });
 
-// *** USERS PAGE ***
-
-// Get a users page
-app.get("/users/:userID", (req, res) => {
-  // Create a new post and add to the list
-  const command = `SELECT * FROM tweets INNER JOIN users ON author = user_id WHERE author = $1`;
-  const params = [req.params.userID];
-
-  db.query(command, params, (err, response) => {
-    if (err) {
-      console.error(err);
-      return err;
-    }
-
-    const tweets = response.rows;
-    const author = {
-      user : tweets[0].author,
-      name : tweets[0].name, 
-      descr: tweets[0].descr 
-    };
-
-    res.render('user', {author: author, tweets: tweets.slice().reverse() });
-  });
-});
-
-// Search for a tweet
-app.get("/tweets/:id", (req, res) => {
-  const command = `SELECT * FROM tweets WHERE id = $1`;
-  const params = [req.params.id];
-
-  db.query(command, params, (err, response) => {
-    if (err) {
-      console.error(err);
-      return err;
-    }
-
-    const tweet = response.rows;
-
-    // console.log(tweets[0].likes);
-
-    res.send(tweet);
-  });
-});
-
 // Add like to a tweet
 app.post('/tweets/:id/like', (req, res) => {
-  const command = `UPDATE tweets SET likes = likes + 1 WHERE id = $1`;
-  const params = [req.params.id];
+  db.toggleLike(user.id, req.params.id);
 
-  db.query(command, params, (err, response) => {
-    if (err) {
-      console.error(err);
-      return err;
-    }
-
-    console.log(response);
-    res.status(200).send({message: 'LIKED TWEET'});
-  });
-
+  res.redirect("/");
 });
 
 // Retweet
@@ -138,8 +95,6 @@ app.post('/tweets/:id/rt', (req, res) => {
   let command_ = `UPDATE tweets SET rts = rts + 1 WHERE id = $1`;
   let params = [req.params.id];
 
-  console.log('Ola');
-  // *** LEMBRAR DE FAZER UMA TRANSACAO RT -> ADD RT TABLE ***
   db.query(command_, params, (err, response) => {
     if (err) {
       console.error(err);
@@ -182,39 +137,52 @@ app.post('/delete/:tweetID', (req, res) => {
 
 });
 
-app.listen(3000, () => {
-  console.log("Listening on 3000");
+// Search for a tweet
+app.get("/tweets/:id", (req, res) => {
+  const command = `SELECT * FROM tweets WHERE id = $1`;
+  const params = [req.params.id];
+
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
+    const tweet = response.rows;
+
+    // console.log(tweets[0].likes);
+
+    res.send(tweet);
+  });
+});
+
+// *** USERS PAGE ***
+
+// Get a users page
+app.get("/users/:userID", (req, res) => {
+  // Create a new post and add to the list
+  const command = `SELECT * FROM tweets INNER JOIN users ON author = user_id WHERE author = $1`;
+  const params = [req.params.userID];
+
+  db.query(command, params, (err, response) => {
+    if (err) {
+      console.error(err);
+      return err;
+    }
+
+    const tweets = response.rows;
+    const author = {
+      user : tweets[0].author,
+      name : tweets[0].name, 
+      descr: tweets[0].descr 
+    };
+
+    res.render('user', {author: author, tweets: tweets.slice().reverse() });
+  });
 });
 
 
 
-
-
-/* class Tweet {
-  constructor(id, author, name, content) {
-
-    this.user = {
-      author: author,
-      name: name
-    }
-    this.id = id;
-    this.date = new Date();
-    this.content = content;
-
-    this.likes = 0;
-    this.rt = 0;
-    this.comments = 0;
-  }
-
-  get time() {
-    const options = { month: 'long', day: 'numeric' };
-    return (new Date(this.date)).toLocaleString('en-US', options)
-  }
-}
-
-let db = JSON.parse(fs.readFileSync('defaultTweets.json'));
-
-let user = {
-  author: 'user123',
-  name: 'Guest'
-} */
+app.listen(3000, () => {
+  console.log("Listening on 3000");
+});
--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 // Database made with PostgreSQL
 
-const {Pool, Client} = require('pg');
+const { Pool, Client } = require('pg');
 
 // Get credentials
 const pool = new Pool({
@@ -8,25 +8,96 @@ const pool = new Pool({
     database: 'twitter'
 });
 
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+});
+
 function addUser(ID, name, descr) {
-    const command = 'INSERT INTO users VALUES ($1, $2, $3)' 
+    const command = 'INSERT INTO users VALUES ($1, $2, $3)'
     const params = [ID, name, descr];
 
     pool.query(command, params, (err, res) => {
-        if(err) {
+        if (err) {
             console.log("Erro no login");
             console.error(err);
         }
-        
+
         return "DEU BOOOOOM";
     });
 }
 
+function toggleLike(userID, tweetID) {
+    const command = 'SELECT * FROM likes WHERE author = $1 AND tweet = $2';
+    const params = [userID, tweetID];
+
+    pool.query(command, params, (err, res) => {
+        if (err) {
+            console.error("Erro em like\n", err);
+            return err;
+        }
+
+        if (!res.rows.length) {
+            addLike(userID, tweetID);
+        } else {
+            removeLike(userID, tweetID);
+        }
+    });
+}
+
+function addLike(userID, tweetID) {
+    let command = 'INSERT INTO likes VALUES ($1, $2)';
+    let params = [userID, tweetID];
+
+    pool.query(command, params, (err, res) => {
+        if (err) {
+            console.error("Erro em add like\n", err);
+            return err;
+        }
+    });
+
+    command = `UPDATE tweets SET likes = likes + 1 WHERE id = $1`;
+    params = [tweetID];
+
+    pool.query(command, params, (err, res) => {
+        if (err) {
+            console.error(err);
+            return err;
+        }
+    });
+
+    console.log("Liked");
+}
+
+function removeLike(userID, tweetID) {
+    let command = 'DELETE FROM likes WHERE author = $1 AND tweet = $2';
+    let params = [userID, tweetID];
+
+    pool.query(command, params, (err, res) => {
+        if (err) {
+            console.error("Erro em remove like\n", err);
+            return err;
+        }
+    });
+
+    command = `UPDATE tweets SET likes = likes - 1 WHERE id = $1`;
+    params = [tweetID];
+
+    pool.query(command, params, (err, res) => {
+        if (err) {
+            console.error(err);
+            return err;
+        }
+    });
+
+    console.log("Removeu like");
+}
 
 module.exports = {
     query: (text, params, callback) => {
         return pool.query(text, params, callback);
     },
 
-    addUser: addUser
+    addUser: addUser,
+    toggleLike: toggleLike
 }
